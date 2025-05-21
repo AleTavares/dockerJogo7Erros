4f3585af2854f2c2455f34af2306060a826a164e
Felipwz <luizfelipesiqueira83@gmail.com>
2025-05-20 21:37:37 -0300

    Correção de todos os erros juntamente com o md

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 // Configuração do MongoDB
-const mongoURI = 'mongodb://mongodb:27017/desafio';
+const mongoURI = 'mongodb://mongo:27017/desafio';
 
 // Conexão com o MongoDB
 mongoose.connect(mongoURI)
